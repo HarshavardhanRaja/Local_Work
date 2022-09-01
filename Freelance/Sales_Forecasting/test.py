12787c8daf9b8bb13f98bb1e0ad3c23603d2e8f1
Harshavardhan Raja <harsha@Harshas-MacBook-Air.local>
2022-09-01 20:12:36 +0530

    5th Commit on 1 Sept 2022

--- Freelance/Sales_Forecasting/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-s1 = pd.read_excel('Sales-Forecasting/2 years data.xlsx', sheet_name='Planilha1')
-s2 = pd.read_excel('Sales-Forecasting/2 years data.xlsx', sheet_name='Price increase dates')
-s3 = pd.read_excel('Sales-Forecasting/2 years data.xlsx', sheet_name='Mandatory vacation dates')
-
-print(s1.head())
